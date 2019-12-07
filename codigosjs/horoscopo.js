8ddff00c2a4c08ae67f5d583dcb9639541b4c0be
Elkin Jimenez <elomjiga@hotmail.com>
2019-12-07 12:00:00 -0500

    Ejemplo del horóscopo

---
+++ codigosjs/horoscopo.js
@@ -0,0 +1,16 @@
+var signo = prompt('Cúal es tu signo? : ')
+
+switch (signo) {
+
+    case 'Acuario':
+        alert('Horóscopo de acuario')
+        break
+    case 'Leo':
+        alert('Horóscopo de leo')
+        break
+    case 'Virgo':
+    case 'Virgo1':
+        alert('Horóscopo de virgo')
+        break
+
+}
