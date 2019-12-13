4dad971463f2b61dc128783583e89dd1b914d47b
Elkin Jimenez <elomjiga@hotmail.com>
2019-12-13 16:06:57 -0500

    Finalización de asincronismo

--- codigosjs/callbacks.js
+++ codigosjs/callbacks.js
@@ -2,11 +2,69 @@ const API_URL = 'https://swapi.co/api/'
 
 const PEOPLE_URL = 'people/:id'
 
-const lukeURL = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const opts = { crossDomain: true }
 
-const onPeopleResponse = function (persona) {
-    console.log(`Hola, soy ${persona.name}`)
+function obtenerPersonaje(id) {
+
+    return new Promise(function (resolve, reject) {
+        const URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $.get(URL, opts, function (data) {
+            resolve(data)
+        }).fail(() => reject(id))
+    })
+
 }
 
-$.get(lukeURL, opts, onPeopleResponse)
+function onError(id) {
+    console.log(`Error al obtener el personaje ${id}`)
+}
+
+async function obtenerPersonajes() {
+
+    var ids = [1, 2, 3, 4];
+
+    var promesas = ids.map(id => obtenerPersonaje(id))
+
+    try {
+
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+
+    } catch (id) {
+
+        onError(id);
+
+    }
+
+}
+
+
+obtenerPersonajes();
+
+
+// obtenerPersonaje(1)
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(2)
+//     })
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(3)
+//     })
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(4)
+//     })
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(5)
+//     })
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(6)
+//     })
+//     .then(personaje => {
+//         console.log(`Hola, soy ${personaje.name}`)
+//         return obtenerPersonaje(7)
+//     })
+//     .catch(onError)
